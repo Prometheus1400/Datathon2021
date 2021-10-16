e2dac9f8ccab96b6f918ed6f76398449ab43c2c6
Prometheus1400 <prometheus1400@gmail.com>
2021-10-16 15:31:22 -0500

    updated

--- preprocessing/gen_base_database.py
+++ preprocessing/gen_base_database.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import snowflake.connector
 from user_info import user, password, account
+import pandas as pd
+import tqdm
 #must create user_info with user, password, account value yourself
 
 # Gets the version
@@ -9,15 +11,55 @@ ctx = snowflake.connector.connect(
     password=password,
     account=account
     )
-cs = ctx.cursor().execute("SELECT * FROM US_STOCKS_DAILY.PUBLIC.STOCK_HISTORY WHERE symbol='AAPL' ORDER BY date")
-result = cs.fetchmany(10)
-for pt in result:
-    print(pt)
 
-# try:
-#     cs.execute("SELECT current_version()")
-#     one_row = cs.fetchone()
-#     print(one_row[0])
-# finally:
-#     cs.close()
-# ctx.close()
+def year_difference(year: int) -> int:
+    query_string = f"SELECT * FROM US_STOCKS_DAILY.PUBLIC.STOCK_HISTORY WHERE symbol='AAPL' AND date >= '{year}-01-01' AND date < '{year+1}-1-1' ORDER BY date"
+    data_cursor = ctx.cursor().execute(query_string)
+    data = data_cursor.fetchall()
+
+    year_start = data[0][3] #opening stock price for the year
+    year_end = data[-1][6] #closing stock price for the year
+    return year_start - year_end
+
+def get_inc_dec(diff:int) -> int:
+    """
+    converts yearly difference into 1 (for annual increase) and 0 (for annual decrease)
+    """
+    if (diff <= 0):
+        return 0
+    else:
+        return 1
+
+def get_info_list(ticker:str, low_year:int, high_year:int) -> list(tuple):
+    """
+    gets info between low_year and high year inclusive
+    """
+    info_cursor = ctx.cursor().execute(f"SELECT * FROM US_STOCKS_DAILY.PUBLIC.STOCK_HISTORY WHERE symbol='{ticker}' AND date<'{high_year+1}-1-1' AND date>='{low_year}-1-1' ORDER BY date")
+    return info_cursor.fetchall()
+
+def get_important_features(point:tuple) -> dict:
+    
+
+def main():
+    prediction_year = 2019
+    train_range = (2008, 2018)
+    datalist = []
+    # tickerList = []
+    ticker = 'AAPL'
+    
+    infoList = get_info_list(ticker, train_range[0], train_range[1])
+    target = get_inc_dec(year_difference(prediction_year))
+        
+
+    infoList = info.fetchall()
+
+    for pnt in result:
+        d = dict()
+        print(pnt)
+        
+
+    
+
+
+if __name__=="__main__":
+    main()
